2202d21bfa7c368c9e90e56784b78c40f09d67c0
Prasanna Gaikwad <prasannagaikwad0018@gmail.com>
2024-12-15 13:50:46 +0530

    resume edited

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     { name: 'Skills', to: 'skills' },
     { name: 'Projects', to: 'projects' },
     { name: 'Contact', to: 'contact' },
-    { name: 'Resume', to: "/public/resume.pdf", download: "MY_RESUME.PDF" }
+    { name: 'Resume', to: "/public/resume.pdf", download: "Prasanna_Gaikwad_Resume_Pdf" }
 
   ];
 

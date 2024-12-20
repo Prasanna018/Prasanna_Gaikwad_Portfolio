42db8c1f0fb916be45c18ff1a0373b602722a385
Prasanna Gaikwad <prasannagaikwad0018@gmail.com>
2024-12-20 18:33:06 +0530

    scrool

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,41 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import './index.css';
+import { motion, useScroll, useSpring } from 'framer-motion';
 
 function App() {
+  const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  })
+
   return (
-    <div className="bg-primary">
-      <Navbar />
-      <Home />
-      <Skills />
-      <Projects />
-      <Contact />
-    </div>
+    <>
+      <motion.div
+        style={{
+          scaleX,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 10,
+          originX: 0,
+          zIndex: 100,
+          backgroundColor: "#f30283",
+        }}
+      />
+      <div className="bg-primary"
+
+      >
+        <Navbar />
+        <Home />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div >
+    </>
   );
 }
 

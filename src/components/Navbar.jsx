66dcc7db6d6629fda093e86d4bde8025f9ef3023
Prasanna Gaikwad <prasannagaikwad0018@gmail.com>
2025-02-03 22:10:36 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,33 @@
 import { useState } from 'react';
-import { Link, scroller } from 'react-scroll';
+import { Link } from 'react-scroll';
 import { motion } from 'framer-motion';
 import React from 'react';
 import { resumeUrl } from '../common.jsx/common';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedTab, setSelectedTab] = useState('Home'); // State for selected tab
 
   const navItems = [
     { name: 'Home', to: 'home' },
     { name: 'Skills', to: 'skills' },
     { name: 'Projects', to: 'projects' },
-    // { name: 'Experience', to: 'experience' },
     { name: 'Contact', to: 'contact' },
     { name: 'Resume', to: resumeUrl, download: 'Resume' },
   ];
 
-  // Handle tab click to update state and scroll to the section
-  const handleTabClick = (name) => {
-    setSelectedTab(name);
-    setIsOpen(false); // Close mobile menu on selection
-  };
-
   return (
-    <nav className="fixed w-full  backdrop-blur-sm z-50">
+    <nav className="fixed w-full backdrop-blur-sm z-50">
       <div className="max-w-6xl mx-auto px-4">
-        <div className="flex justify-between items-center h-16 ">
+        <div className="flex justify-between items-center h-16">
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
             className="text-secondary font-bold text-xl cursor-pointer"
           >
-
-
             <Link to="home" smooth={true} duration={500}>
               Prasanna Gaikwad
             </Link>
-
           </motion.div>
 
           {/* Desktop Menu */}
@@ -56,15 +45,10 @@ const Navbar = () => {
                   </a>
                 ) : (
                   <Link
-                    onClick={() => handleTabClick(item.name)}
                     to={item.to}
                     smooth={true}
                     duration={500}
-                    spy={true} // Watches the scroll position
-                    activeClass="underline text-secondary" // Add underline dynamically
-                    onSetActive={() => setSelectedTab(item.name)} // Update state when active
-                    className={`text-textSecondary hover:text-secondary hover:scale-110 cursor-pointer ${selectedTab === item.name ? 'underline text-secondary' : ''
-                      }`}
+                    className="text-textSecondary hover:text-secondary hover:scale-110 cursor-pointer"
                   >
                     {item.name}
                   </Link>
@@ -79,26 +63,11 @@ const Navbar = () => {
               onClick={() => setIsOpen(!isOpen)}
               className="text-textSecondary hover:text-secondary"
             >
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
+              <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 {isOpen ? (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                 ) : (
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 )}
               </svg>
             </button>
@@ -127,15 +96,10 @@ const Navbar = () => {
                     </a>
                   ) : (
                     <Link
-                      onClick={() => handleTabClick(item.name)}
                       to={item.to}
                       smooth={true}
                       duration={500}
-                      spy={true} // Watches the scroll position
-                      activeClass="underline text-secondary" // Add underline dynamically
-                      onSetActive={() => setSelectedTab(item.name)} // Update state when active
-                      className={`text-textSecondary hover:text-secondary cursor-pointer hover:scale-105 ${selectedTab === item.name ? 'underline text-secondary' : ''
-                        }`}
+                      className="text-textSecondary hover:text-secondary cursor-pointer hover:scale-105"
                     >
                       {item.name}
                     </Link>

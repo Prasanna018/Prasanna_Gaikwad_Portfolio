edbb24d06b177c594f846363f0aaa716763f02c7
Prasanna Gaikwad <prasannagaikwad0018@gmail.com>
2025-02-13 19:30:38 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed w-full backdrop-blur-sm z-50">
-      <div className="max-w-6xl mx-auto px-4">
+      <div className="max-w-6xl mx-auto px-6">
         <div className="flex justify-between items-center h-18">
           <motion.div
             initial={{ opacity: 0 }}
